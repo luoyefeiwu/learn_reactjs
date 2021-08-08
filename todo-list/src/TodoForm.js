20ddc282d20b426173f5f1d861a6f9a1dd1c9be0
huangchuanwang <568384493.com>
2021-08-08 23:09:24 +0800

    组件化

---
+++ todo-list/src/TodoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class TodoForm extends React.Component {
+    addTodoItem = () => {
+        this.props.addTodoItem(this.refs.todoItemValue.value);
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" ref="todoItemValue" placeholder="add or search something..."></input>
+                <button type="submit" onClick={this.addTodoItem}>添加</button>
+            </div>
+        )
+    }
+}

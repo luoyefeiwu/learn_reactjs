baa2ac92b8a30ea261f0e5ffc6cc8633219fff92
huangchuanwang <568384493@qq.com>
2021-08-09 18:53:57 +0800

    路由

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Router, Route } from 'react-router';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={App}/>
+  </Router>
+), document.getElementById('app'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
